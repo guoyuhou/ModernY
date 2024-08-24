08742dfaf52fab61bc8bdeca0b3418abc8474d9f
Alexander <17806067729@163.com>
2024-08-24 21:54:56 +0800

    2024.8.24_032

--- PyGWalker.py
+++ PyGWalker.py
@@ -6,7 +6,7 @@ import os
 st.set_page_config(
     page_title = 'PyGWalker',
     layout='wide'
-)
+)   
 
 st.title('PyGWalker')
 
@@ -16,15 +16,14 @@ uploaded_file = st.file_uploader("选择你的CSV文件", type=["csv"])
 if uploaded_file is not None:  
     # 使用Pandas读取上传的CSV文件  
     data = pd.read_csv(uploaded_file)  
-      
+  
     # 显示数据帧的前几行  
-    st.write(data.head())
-
-@st.cache_resource
-def get_pyg_renderer():
-    df = pd.read_csv(data)
-    return StreamlitRender(df, spec='./gw_config.json', spec_io_mode='rw')
-
-renderer = get_pyg_renderer()
-
-renderer.explorer()
+    st.write(data.head())  
+  
+    # 定义一个函数来获取渲染器，确保在文件上传后才调用  
+    @st.cache_resource  
+    def get_pyg_renderer(df):  
+        return StreamlitRender(df, spec='./gw_config.json', spec_io_mode='rw')  
+  
+    renderer = get_pyg_renderer(data)  
+    renderer.explorer()
