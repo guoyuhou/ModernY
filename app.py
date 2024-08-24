8ae5cb5c855d9856572be04b44405cac3ba87a5e
Alexander <17806067729@163.com>
2024-08-24 21:43:17 +0800

    2024.8.24_030

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ print('=========================================================================
 def page():
     pages = {
         'ModernY': 'ModernY_page.py',
+        'PyGWalker' : 'PyGWalker.py',
         'Tools': {
-            'Github' : 'ModernY_tools/Github.py',
-            'Command': 'ModernY_tools/Command.py',
-            'PyQt5': 'ModernY_tools/PyQt5.py',
-            'Strom Genie': 'ModernY_tools/Storm_genie.py',
-            'Markdown': 'ModernY_tools/Markdown.py',
-            'smalltools': 'ModernY_tools/smalltools.py'   
+            'Github' : 'ModernY_tools\Github.py',
+            'Command': 'ModernY_tools\Command.py',
+            'PyQt5': 'ModernY_tools\PyQt5.py',
+            'Strom Genie': 'ModernY_tools\Storm_genie.py',
+            'Markdown': 'ModernY_tools\Markdown.py',
+            'Smalltools': 'ModernY_tools\smalltools.py'
         }
     }
     page_name = st.sidebar.radio('Navigation', list(pages.keys()))
     page_file = None
 
     if page_name == 'ModernY':
+        page_file = pages[page_name]
+    if page_name == 'PyGWalker':
         page_file = pages[page_name]
     if page_name == 'Tools':
         page = pages[page_name]
