00a6296f4a982d32d07295087639a9d00833ce64
Alexander <17806067729@163.com>
2024-08-25 22:29:28 +0800

    2024.8.25_010

---
+++ Blogs.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+st.title('Blogs')
+st.write('2024.8.18 开始')
+st.text('https://zhuanlan.zhihu.com/p/715119821')
+st.write('2024.8.20')
+st.text('https://zhuanlan.zhihu.com/p/715614431')
+st.write('再回首，轻舟已过万重山')
+st.text('https://zhuanlan.zhihu.com/p/716435221')
