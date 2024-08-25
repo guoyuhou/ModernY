156e1f35bc0ce23f4c376bae5a27aed90fcaa6c2
Alexander <17806067729@163.com>
2024-08-25 22:38:11 +0800

    2024.8.25_014

--- Blogs.py
+++ Blogs.py
@@ -3,9 +3,6 @@ import os
 
 st.title('Blogs')
 
-st.write('2024.8.18 开始')
-st.text('https://zhuanlan.zhihu.com/p/715119821')
-st.write('2024.8.20')
-st.text('https://zhuanlan.zhihu.com/p/715614431')
-st.write('再回首，轻舟已过万重山')
-st.text('https://zhuanlan.zhihu.com/p/716435221')
+st.write('2024.8.18 开始', 'https://zhuanlan.zhihu.com/p/715119821')
+st.write('2024.8.20', 'https://zhuanlan.zhihu.com/p/715614431')
+st.write('再回首，轻舟已过万重山', 'https://zhuanlan.zhihu.com/p/716435221')
