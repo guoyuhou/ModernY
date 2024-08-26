db7f6833e6c74104031a3dad8620a66a2aad9b11
Alexander <17806067729@163.com>
2024-08-26 10:54:47 +0800

    2024.8.27_009

--- ModernY_tools/Markdown.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import os
-
-st.title('Markdown')
-st.text(f'1. Title:'
-         '\n # First level title'
-         '\n ## Second level title'
-         '\n ### Third level title')
-
