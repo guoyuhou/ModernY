6d4fd67f09ccc23c45dfaf0e091264dbf69696e1
Alexander <17806067729@163.com>
2024-09-08 17:57:20 +0800

    2024.9.8_008

--- app.py
+++ app.py
@@ -4,6 +4,58 @@ import os
 import pygwalker
 from pygwalker.api.streamlit import StreamlitRenderer
 import time
+import oss2
+from io import BytesIO
+
+
+# 从Streamlit的Secrets中读取OSS的密钥和存储桶信息
+ACCESS_KEY_ID = st.secrets["oss"]["OSS_ACCESS_KEY_ID"]
+ACCESS_KEY_SECRET = st.secrets["oss"]["OSS_ACCESS_KEY_SECRET"]
+BUCKET_NAME = st.secrets["oss"]["OSS_ENDPOINT"]
+ENDPOINT = st.secrets["oss"]["OSS_BUCKET_NAME"]
+
+# 创建OSS认证和存储桶对象
+auth = oss2.Auth(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
+bucket = oss2.Bucket(auth, ENDPOINT, BUCKET_NAME)
+
+def list_files():
+    files = []
+    for obj in oss2.ObjectIterator(bucket):
+        files.append(obj.key)
+    return files
+
+def display_files():
+    st.subheader('文件列表')
+    files = list_files()
+    if files:
+        for file in files:
+            st.write(file)
+    else:
+        st.write('没有文件可显示')
+
+def upload_file():
+    st.subheader('上传文件')
+    uploaded_file = st.file_uploader("选择要上传的文件")
+    if uploaded_file:
+        file_name = uploaded_file.name
+        bucket.put_object(file_name, uploaded_file)
+        st.success(f'文件 {file_name} 上传成功')
+
+def download_file():
+    st.subheader('下载文件')
+    files = list_files()
+    file_name = st.selectbox('选择要下载的文件', files)
+    if file_name:
+        if st.button('下载'):
+            obj = bucket.get_object(file_name)
+            file_data = obj.read()
+            st.download_button(
+                label='下载文件',
+                data=file_data,
+                file_name=file_name,
+                mime='application/octet-stream'
+            )
+
 
 
 def page():
@@ -20,7 +72,8 @@ def page():
             'Storm Genie': os.path.join('ModernY_tools', 'Storm_genie.py'),
             'Smalltools': os.path.join('ModernY_tools', 'smalltools.py'),
             'Chatpaper': os.path.join('ModernY_tools', 'Chatpaper.py')
-        }
+        },
+        'Cloud Storage': 'cloud_storage.py'  # 新增页面
     }
 
     page_name = st.sidebar.radio('Navigation', list(pages.keys()))
@@ -40,6 +93,11 @@ def page():
         tool = pages[page_name]
         page_title = st.sidebar.radio('Classification', list(tool.keys()))
         page_file = tool[page_title]
+    elif page_name == 'Cloud Storage':
+        st.sidebar.write('云存储管理')
+        display_files()
+        upload_file()
+        download_file()
     
     if page_file:
         try:
---
+++ cloud_storage.py
@@ -0,0 +1,3 @@
+import streamlit as st
+import os
+st.title('Cloud storage')
